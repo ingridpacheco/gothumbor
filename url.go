dc406e76f5dc27f6b44278c0e1ce3b07850789c6
Ingrid Pacheco <ingrid.pacheco@corp.globo.com>
2020-09-04 16:38:59 -0300

    fix: change fitin position to match current rule

--- url.go
+++ url.go
@@ -55,6 +55,10 @@ func getURLParts(imageURL string, options ThumborOptions) (urlPartial string, er
 		parts = append(parts, "meta")
 	}
 
+	if options.FitIn {
+		parts = append(parts, "fit-in")
+	}
+
 	if options.Left != 0 || options.Top != 0 || options.Right != 0 || options.Bottom != 0 {
 		parts = append(parts, fmt.Sprintf("%dx%d:%dx%d", options.Left, options.Top, options.Right, options.Bottom))
 	}
@@ -82,10 +86,6 @@ func getURLParts(imageURL string, options ThumborOptions) (urlPartial string, er
 	filters := []string{}
 	for _, value := range options.Filters {
 		filters = append(filters, value)
-	}
-
-	if options.FitIn {
-		parts = append(parts, "fit-in")
 	}
 
 	if len(options.Filters) > 0 {
